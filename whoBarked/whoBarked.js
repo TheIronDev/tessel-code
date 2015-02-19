f65f28391da37cee711b70b1a15c67363403f2f5
Tyler Stark <theirondeveloper@gmail.com>
2015-02-18 21:59:28 -0800

    WhoBarked: Camera Support

--- whoBarked/whoBarked.js
+++ whoBarked/whoBarked.js
@@ -4,15 +4,33 @@ var tessel = require('tessel'),
 	cameraLib = require('camera-vc0706');
 
 var ambient = ambientlib.use(tessel.port['A']),
-	camera = cameraLib.use(tessel.port['D']),
-	notificationLED = tessel.led[3];
+	camera = cameraLib.use(tessel.port['B']),
+	notificationLED = tessel.led[3],
+	AMBIENT_SOUND_TRIGGER_LEVEL = 0.05;
 
 function handleError(err) {
 	console.error(err);
 }
 
 function ambientReadyHandler() {
-	ambient.setSoundTrigger(0.05);
+	ambient.setSoundTrigger(AMBIENT_SOUND_TRIGGER_LEVEL);
+}
+
+function takePicture(newDate) {
+	notificationLED.high();
+	camera.takePicture(function(err, image) {
+		if (err) {
+			return console.log('error taking image', err);
+		}
+
+		notificationLED.low();
+		// Name the image
+		var name = 'picture-' + newDate.toISOString() + '.jpg';
+		// Save the image
+		console.log('Picture saving as', name, '...');
+		process.sendfile(name, image);
+		console.log('done.');
+	});
 }
 
 function ambientSoundHandler(data) {
@@ -22,11 +40,11 @@ function ambientSoundHandler(data) {
 	// Clear it
 	ambient.clearSoundTrigger();
 
+	takePicture(newDate);
+
 	//After 1.5 seconds reset sound trigger
 	setTimeout(function () {
-
-		ambient.setSoundTrigger(0.05);
-
+		ambient.setSoundTrigger(AMBIENT_SOUND_TRIGGER_LEVEL);
 	},1500);
 
 }
@@ -36,25 +54,7 @@ ambient.on('sound-trigger', ambientSoundHandler);
 
 
 camera.on('ready', function() {
-	// Camera integration is our next step
-	return true;
-	notificationLED.high();
-	// Take the picture
-	camera.takePicture(function(err, image) {
-		if (err) {
-			console.log('error taking image', err);
-		} else {
-			notificationLED.low();
-			// Name the image
-			var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
-			// Save the image
-			console.log('Picture saving as', name, '...');
-			process.sendfile(name, image);
-			console.log('done.');
-			// Turn the camera off to end the script
-			camera.disable();
-		}
-	});
+	// Should I do anything here?  My assumption is lets just leave the camera running.
 });
 
 ambient.on('error', handleError);
